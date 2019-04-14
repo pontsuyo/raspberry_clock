dc718674ade762ac6408c58f0d70e7a3bab712d7
pontsuyo <tsuyopon9221@gmail.com>
2019-04-14 23:13:32 +0900

    first commit

---
+++ lchika.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+for x in range(5):
+    GPIO.output(2,True)
+    time.sleep(2)
+    GPIO.output(2,False)
+    time.sleep(2)
+GPIO.cleanup()
